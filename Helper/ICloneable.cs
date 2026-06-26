ad916de7470fdd283f987b577f4375728295131c
agrdino <>
2026-06-26 10:35:05 +0700

    .

--- Helper/ICloneable.cs
+++ Helper/ICloneable.cs
@@ -1,19 +1,7 @@
-using System.Collections.Generic;
-
 namespace qtLib.Helper
 {
     public interface ICloneable<T> where T : class
     {
         public T Clone();
-    }
-
-    public static partial class GameHelper
-    {
-        public static List<T> Clone<T>(this List<T> listToClone) where T : class, ICloneable<T>
-        {
-            List<T> result = new List<T>();
-            listToClone.ForEach(target => result.Add(target.Clone()));
-            return result;
-        }
     }
 }
