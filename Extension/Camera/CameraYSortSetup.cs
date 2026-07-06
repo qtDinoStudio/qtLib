7ceb457e2b0270aa4bc9f2e7c6a262328f6cf4cf
agrdino <>
2026-07-06 15:02:21 +0700

    .

---
+++ Extension/Camera/CameraYSortSetup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace qtLib.Extension.Camera
+{
+    public class CameraYSortSetup : MonoBehaviour
+    {
+        [SerializeField] private UnityEngine.Camera _camera;
+
+        private void Awake()
+        {
+            _camera.transparencySortMode = TransparencySortMode.CustomAxis;
+            _camera.transparencySortAxis = Vector3.up;
+        }
+    }
+}
